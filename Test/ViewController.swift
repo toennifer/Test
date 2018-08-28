b8f611c7ec196fcd80b4ee5c6a4b45bf3c21b978
Jennifer Chou <tzuyun@umich.edu>
2018-08-28 16:22:45 +0800

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Jennifer Chou on 8/28/18.
+//  Copyright © 2018 Jennifer Chou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
